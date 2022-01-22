8b2efb2966adfa4fb6bc484ddd75851307a075ad
Muhammad Iqbal Imani Atfan <42773954+Iotatfan@users.noreply.github.com>
2022-01-22 10:45:36 +0700

    first commit

---
+++ components/button/Button.vue
@@ -0,0 +1,54 @@
+<template>
+  <button
+    type="button"
+    class="io-btn"
+    :class="[buttonClass]"
+    :disabled="disabled"
+    @click="handleClick"
+  >
+  <span class="btn-label">
+    {{ label }}
+  </span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "IoButton",
+  props: {
+      label: {
+          type: String,
+          default: () => {
+              return ""
+          }
+      },
+      buttonType: {
+        type: String,
+        required: true,
+        default: () => {
+              return ""
+          }
+      }
+      // btnIndex: {
+      //     type: Number,
+      //     default: () => {
+      //         return 0
+      //     }
+      // }
+  },
+  methods: {
+    handleClick() {
+      this.$emit('click')
+    }
+  }
+};
+</script>
+
+<style scoped>
+.btn-label  {
+  font-style: normal;
+  font-weight: bold;
+  font-size: 1rem;
+  vertical-align: middle;
+}
+</style>
